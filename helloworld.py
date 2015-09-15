dfa6d45257f439a939592a59c0ff91dac14ad28c
danwardvs <danwardvs@adsgames.net>
2015-09-15 18:32:59 -0400

    Basic radio interface started

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 # set up pygame
 pygame.init()
-
+frequency = 100.0
 
 # set up the window
 windowSurface = pygame.display.set_mode((320, 240), 0, 32)
@@ -20,16 +20,19 @@ BLUE = (0, 0, 255)
 basicFont = pygame.font.SysFont(None, 48)
 
 # set up the text
-text = basicFont.render('Freq:', True, WHITE, BLUE)
+text = basicFont.render('Freq:' + str(frequency), True, WHITE, BLUE)
 textRect = text.get_rect()
-textRect.centerx = 100
-textRect.centery = 20
+textRect.centerx = 110
+textRect.centery = 120
 
 # draw the white background onto the surface
 windowSurface.fill(WHITE)
 
 # draw the text's background rectangle onto the surface
 pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
+pygame.draw.rect(windowSurface, BLACK, (220,10,90,60))
+pygame.draw.rect(windowSurface, BLACK, (220,90,90,60))
+pygame.draw.rect(windowSurface, BLACK, (220,170,90,60))
 
 # draw the text onto the surface
 windowSurface.blit(text, textRect)
@@ -39,7 +42,12 @@ pygame.display.update()
 
 # run the game loop
 while True:
+
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type ==pygame.MOUSEBUTTONDOWN:
+            
+            
