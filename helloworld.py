5a8232224e3616d3696de7b4504a735e7a2a8a0e
danwardvs <danwardvs@adsgames.net>
2015-09-15 18:40:59 -0400

    Rect and broken collison stuffs

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,10 @@ from pygame.locals import *
 pygame.init()
 frequency = 100.0
 
+upButton = Rect(220,10,90,60)
+downButton = Rect(220,90,90,60)
+playButton = Rect(220,170,90,60)
+
 # set up the window
 windowSurface = pygame.display.set_mode((320, 240), 0, 32)
 pygame.display.set_caption('Hello world!')
@@ -30,9 +34,9 @@ windowSurface.fill(WHITE)
 
 # draw the text's background rectangle onto the surface
 pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
-pygame.draw.rect(windowSurface, BLACK, (220,10,90,60))
-pygame.draw.rect(windowSurface, BLACK, (220,90,90,60))
-pygame.draw.rect(windowSurface, BLACK, (220,170,90,60))
+pygame.draw.rect(windowSurface, BLACK, upButton)
+pygame.draw.rect(windowSurface, BLACK, downButton)
+pygame.draw.rect(windowSurface, BLACK, playButton)
 
 # draw the text onto the surface
 windowSurface.blit(text, textRect)
@@ -43,11 +47,21 @@ pygame.display.update()
 # run the game loop
 while True:
 
-    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        if event.type ==pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x,y = event.pos
+            if upButton.collidepoint(x,y):
+                frequency = frequency + 0.1
+                
+    # draw the text onto the surface
+    windowSurface.blit(text, textRect)
+
+    # draw the window onto the screen
+    pygame.display.update()
+    
+            
             
             
